232f431422aae5ef5546706ec5140d1085f5ae8c
rsk7 <ravikrishnasingh@gmail.com>
2016-04-17 23:48:47 -0400

    random

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ import randomWords from 'random-words';
 import draggable from './src/draggable';
 
 let wordcount = 5000;
+let boxcount = 10;
 
 function createBox() {
   let box = document.createElement("div");
   box.setAttribute("class", "box");
+  box.style.top = Math.random() * 10000 % (window.innerHeight - 200);
+  box.style.left = Math.random() * 10000 % (window.innerWidth - 200);
+  let dimension = Math.random() * 1000 % 200;
+  dimension = (dimension < 50) ? 50 : dimension;
+  box.style.height = dimension;
+  box.style.width = dimension;
   return draggable(box);
 }
 
@@ -15,11 +22,14 @@ document.addEventListener("DOMContentLoaded", () => {
   let words = randomWords({ exactly: wordcount, join: ' '});
   let shape = document.createElement("div");
 
-  let box = createBox();
+  for (let i = 0; i < boxcount; i++) {
+    let box = createBox();
+    container.appendChild(box);
+  } 
 
   let textContainer = document.createTextNode(words);
-  container.appendChild(box);
   container.appendChild(textContainer);
+
   body.appendChild(container);
 });
 
---
+++ src/resize.js
@@ -0,0 +1,3 @@
+export default function resizable(element) {
+  
+}
